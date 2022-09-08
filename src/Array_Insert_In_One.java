7bde493174c0eb520b79b2e283b93de29bdc2042
Shubham13308 <103982047+Shubham13308@users.noreply.github.com>
2022-09-08 18:01:22 +0530

    First Commit

---
+++ src/Array_Insert_In_One.java
@@ -0,0 +1,10 @@
+public class Array_Insert_In_One {
+    public static void main(String[] args) {
+        int arr[]={1,2,5,5,8,6,7,87,4,7,487,7};
+        int i;
+        for(int x:arr){
+            System.out.println(x);
+        }
+
+    }
+}
