381b5873459b3dafdc309d0e7faaf1b2e036a716
Shubham13308 <103982047+Shubham13308@users.noreply.github.com>
2022-09-08 17:59:59 +0530

    First Commit

---
+++ src/Array_Insert.java
@@ -0,0 +1,17 @@
+public class Array_Insert {
+    public static void main(String[] args) {
+        int arr[]=new int[5];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        arr[3]=4;
+        arr[4]=5;
+
+        System.out.println(arr[0]=1);
+        System.out.println(arr[1]=2);
+        System.out.println(arr[2]=3);
+        System.out.println(arr[3]=4);
+        System.out.println(arr[4]=5);
+
+    }
+}
