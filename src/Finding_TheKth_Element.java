06a10b55f353958dd868caf3b0c61848d899ae2d
Shubham13308 <103982047+Shubham13308@users.noreply.github.com>
2022-09-08 19:02:48 +0530

    First Commit

--- src/Finding_TheKth_Element.java
+++ src/Finding_TheKth_Element.java
@@ -4,17 +4,17 @@ public class Finding_TheKth_Element
     public static void main(String[] args)
     {
         int n;
-        Scanner s = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter no. of elements you want in array:");
-        n = s.nextInt();
+        n = sc.nextInt();
         int a[] = new int[n];
         System.out.println("Enter all the elements:");
         for (int i = 0; i < n; i++)
         {
-            a[i] = s.nextInt();
+            a[i] = sc.nextInt();
         }
         System.out.print("Enter the k th position at which you want to check number:");
-        int k = s.nextInt();
+        int k = sc.nextInt();
         System.out.println("Number:"+a[k-1]);
     }
 }
