38816bf23e6eb71bfa492e2287f47164b1d128fe
Shubham13308 <103982047+Shubham13308@users.noreply.github.com>
2022-09-08 18:22:20 +0530

    First Commit

---
+++ src/Array_TwoD.java
@@ -0,0 +1,29 @@
+public class Array_TwoD {
+    public static void main(String[] args) {
+        int arr[][]=new int[3][3];
+        arr[0][0]=12;
+        arr[0][1]=56;
+        arr[0][2]=28;
+        arr[1][0]=56;
+        arr[1][1]=76;
+        arr[1][2]=56;
+        arr[2][0]=66;
+        arr[2][1]=85;
+        arr[2][2]=35;
+        for(int[] x:arr){
+            System.out.println(arr[0][0]=12);
+            System.out.println(arr[0][1]=56);
+            System.out.println( arr[0][2]=28);
+            System.out.println(arr[1][0]=56 );
+            System.out.println( arr[1][1]=76);
+            System.out.println(arr[1][2]=56);
+            System.out.println(arr[2][0]=66);
+            System.out.println(arr[2][1]=85);
+
+
+
+
+        }
+
+    }
+}
